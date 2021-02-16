e2675f5d7b28367371cbeb84f926cb920c9f0cb5
Wesley-Chok <55215498+Wesley-Chok@users.noreply.github.com>
2021-02-16 00:44:30 -0800

    Update rectangle.cpp

--- Assignment 1: Perimeter of Rectangle/rectangle.cpp
+++ Assignment 1: Perimeter of Rectangle/rectangle.cpp
@@ -1,21 +1,61 @@
+//****************************************************************************************************************************
+//Program name: "Assignment 1 Perimeter of Rectangle".  This program demonstrates the perimeter of the rectangle through     *                                                                        
+//input variables and arithmetic functions. The arithmetic functions primarily used were addition (addsd), multiplication    *
+//(mulsd) and division (divsd) through registers Copyright (C) 2021 Wesley Chok                                              *                                                                             
+//This file is part of the software program "Perimeter of Rectangle".                                                        *
+//Perimeter of Rectangle is free software: you can redistribute it and/or modify it under the terms of the GNU General       *
+//Public License version 3 as published by the Free Software Foundation.                                                     *
+//Perimeter of Rectangle is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the       *
+//implied                                                                                                                    *
+//warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.     *
+//A copy of the GNU General Public License v3 is available here:  <https:;www.gnu.org/licenses/>.                            *
+//****************************************************************************************************************************
+
+
+//=======1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2=========3**
+//
+//Author information
+//  Author name: Wesley Chok
+//  Author email: wchok@csu.fullerton.edu
+//
+//Program information
+//  Program name: Assignment 1 Perimeter of Rectangle
+//  Programming languages: One modules in C++ and one module in X86
+//  Date program began: 20120-Feb-16
+//  Date of last update: 2020-Feb-16
+//  Date of reorganization of comments: 2020-Feb-16
+//  Files in this program: rectangle.cpp, perimeter.asm
+//  Status: Finished.  The program was tested extensively with no errors in Xubuntu19.04.
+//
+//Purpose
+//  Show how to input and output floating point (64-bit) numbers.
+//
+//This file
+//   File name: manage-floats.c
+//   Language: C++
+//   Max page width: 132 columns
+//   Compile: g++ -c -m64 -Wall -fno-pie -no-pie -o rectangle.o rectangle.cpp
+//   Link: g++ -m64 -fno-pie -no-pie -o perimOfRectangle.out rectangle.o perimeter.o -std=c++17 
+//   Optimal print specification: 132 columns width, 7 points, monospace, 8½x11 paper
+//
+//=======1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2=========3**
+//
+//
+//===== Begin code area ===========================================================================================================
+
 #include <stdio.h>
-#include <stdint.h> //For C99 compatibility
+#include <stdint.h>
 #include <time.h>
 #include <sys/time.h>
-#include <stdlib.h>   //For malloc
-//At the time of this writing C99 is the newest available standard for C language.  Most modern C compilers conform to the C99 
-//standard.  The GNU compiler, known as gcc, is C99 compliant.  If both languages, C and X86, are C99 compliant then object code 
-//files from both compilers may be linked to form a single executable. 
-//
-//The standard prototypes for any functions that may be called later.  This main function calls exactly one function.
+#include <stdlib.h> 
 extern "C" double _first();
 //
 int main()
 {
-double answer = 0.0;
- printf("The main function manage-floats.c has begun\n");
- printf("The function floatio will now be called\n");
- answer = _first();
- printf("The function floatio has returned this value %8.5lf.  Have a nice day\n",answer);
- return 0;
+    double answer = 0.0;
+    answer = _first();
+    printf("The main functio received this number %8.5lf and has decided to keep it.\n",answer);
+    printf("A 0 will be returned to the operating system.\n");
+    printf("Have a nice day.");
+    return 0;
 }
