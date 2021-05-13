5abfb336833ee4d1b4772f055ef334e45674da44
Wesley-Chok <55215498+Wesley-Chok@users.noreply.github.com>
2021-05-12 23:19:20 -0700

    Update interview.asm

--- Assignment 4: King of Assembly/Part 1/interview.asm
+++ Assignment 4: King of Assembly/Part 1/interview.asm
@@ -1,3 +1,46 @@
+;****************************************************************************************************************************
+;Program name: "Assignment 4 King of Assembly".  This program demonstrates passing values from main to assembly file,       *
+;calculating arithimetic functions in assembly file and using IEEE 754 conversions.                                         *
+;Copyright (C) 2021 Wesley Chok                                                                                             *
+;This file is part of the software program "King of Assembly".                                                              *
+;King of Assembly is free software: you can redistribute it and/or modify it under the terms of the GNU General             *
+;Public License version 3 as published by the Free Software Foundation.                                                     *
+;King of Assembly is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the             *
+;implied                                                                                                                    *
+;warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.     *
+;A copy of the GNU General Public License v3 is available here:  <https:;www.gnu.org/licenses/>.                            *
+;****************************************************************************************************************************
+
+
+
+
+;========1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2=========3**
+;
+;Author information
+;  Author name: Wesley Chok
+;  Author email: chokwj@csu.fullerton.edu
+;
+;Program information
+;  Program name: Assignment 4 King of Assembly
+;  Programming languages: One modules in C++ and one modules assembly.
+;  Date program began: 2021-April-26
+;  Date of last update: 2021-May-12
+;  Date of reorganization of comments: 2021-May-12
+;  Files in this program: main.cpp, interview.asm
+;  Status: Finished.  The program was tested extensively with no errors in Xubuntu19.04.
+;
+;Purpose
+;  This serves as the main assembly file for the program. The tasks this assembly file complete taking values
+;  that were passed from main, calculating arithmetic values and utilizing and converting IEEE 754 values.
+;This file
+;   File name: control.asm
+;   Language: X86 with Intel syntax.
+;   Max page width: 132 columns
+;   Assemble: nasm -f elf64 -l perimeter.lis -o interview.o interview.asm
+
+
+;===== Begin code area ================================================================================================
+
 extern printf
 extern scanf
 extern atolong
@@ -15,14 +58,8 @@ section .data
      floatFormat db "%lf",0
      resistanceOutputPrompt db "The total resistance is %lf Ohms.",10,0
      majorPrompt db "Were you a computer science major (y or n)?",0
-
-     testPrompt db "This is the number: %lf",10,0
-     testPrompted db "I'm alive and well, yes",10,0
-
-
      exitPrompt db 10,"Thank you.  Please follow the exit signs to the front desk.",10,0
 
-     errorPrompt db "Invalid response please restart.",10,0
 
 section .bss
 
@@ -48,215 +85,181 @@ _first:
      push rbx                                                    ;Backing up rbx
      pushf                                                       ;Backing up rflags
 
-     push qword 0
-
-     mov r14, rdi
-     movsd xmm13, xmm0
-
+     ;============= Begin section to initialize registers========================================================================================================
+     push qword 0     
+     mov r14, rdi                               ;Holds the name inputted in main.cpp
+     movsd xmm13, xmm0                          ;Holds the salary inputted in main.cpp
+     ;============= End section to initialize registers==========================================================================================================
+     
+     ;Displays welcome prompt to the user
      push qword 0
      mov rax, 0
-     mov rdi, welcomePrompt
+     mov rdi, welcomePrompt                     ;"Hello %s. I am Ms Fenster. The interview will begin now."
      mov rsi, r15
      call printf
      pop rax
 
+     ;============= Begin section to ask if user is Chris Sawyer=================================================================================================
+     ;Displays salary prompt to the user
      push qword 99
      mov rax, 1
-     mov rdi, salaryPrompt
+     mov rdi, salaryPrompt                      ;"Wow!  %5.3lf  That's a lot of cash. Who do you think you are, Chris Sawyer (y or n)?"
      movsd xmm0, xmm13
      call printf
      pop rax
 
      push qword 0
-     ;mov rax, 0
-     mov rdi, stringFormat
+     mov rdi, stringFormat                      ;"%s"
      mov rsi, rsp
      call scanf
      pop rax
 
-     mov r15, 'y'
-     cmp rax, r15
-     je chris_sawyer
+     mov r15, 'y'                               ;Move 'y' into register r15
+     cmp rax, r15                               ;Compare register rax with r15
+     je chris_sawyer                            ;If register rax is y and equal to r15, jump to chris_sawyer
 
-     mov r15, 'n'
-     cmp rax, r15
-     je resistance
+     mov r15, 'n'                               ;Move 'n' into register r15
+     cmp rax, r15                               ;Compare register rax with r15
+     je resistance                              ;If register rax is n and equal to r15, jump to resistance
+     ;============= End section to ask if user is Chris Sawyer==================================================================================================
 
-     ; mov r13, 'n'
-     ; cmp rdi, r13
-     ; je programmer
-
-     ; push qword 0
-     ; mov rax, 0
-     ; mov rdi, errorPrompt
-     ; call printf
-     ; pop rax
-     ; jmp exit
-
+     
+     ;============= Begin section for resistance jump===========================================================================================================
      resistance:
+     ;Displays programmer electricity prompt to the user
      push qword 0
      mov rax, 0
-     mov rdi, programmerPrompt
+     mov rdi, programmerPrompt                  ;"Alright.  Now we will work on your electricity"
      call printf
      pop rax
 
+     ;Displays the first resistance prompt to the user
      push qword 99
      mov rax, 0
-     mov rdi, resistanceOnePrompt
+     mov rdi, resistanceOnePrompt               ;"Please enter the resistance of circuit #1 in ohms: "
      call printf
      pop rax
 
+     ;Storing user input for the first resistance prompt
      push qword -1
      mov rax, 0
-     mov rdi, floatFormat
+     mov rdi, floatFormat                       ;"%lf"
      mov rsi, rsp
      call scanf
      movsd xmm12, [rsp]
      pop rax
 
-
      push qword 99
      mov rax, 0
-     mov rdi, resistanceTwoPrompt
+     mov rdi, resistanceTwoPrompt               ;"What is the resistance of circuit #2 in ohms: "
      call printf
      pop rax
 
+     ;Storing user input for the second resistance prompt
      push qword -1
      mov rax, 0
-     mov rdi, floatFormat
+     mov rdi, floatFormat                       ;"%lf"
      mov rsi, rsp
      call scanf
      movsd xmm13, [rsp]
      pop rax
 
-
      push qword 0
-     mov r9, 1
-     cvtsi2sd xmm9, r9
-     mov r8, 1
-     cvtsi2sd xmm8, r8
-     mov r15, 1
-     cvtsi2sd xmm15, r15
+     mov r9, 1                                  ;Move 1 into register r9
+     cvtsi2sd xmm9, r9                          ;Convert r9 into a scalar double precision floating-point
+     mov r8, 1                                  ;Move 1 into register r8
+     cvtsi2sd xmm8, r8                          ;Convert r8 into a scalar double precision floating-point
+     mov r15, 1                                 ;Move 1 into register r15
+     cvtsi2sd xmm15, r15                        ;Convert r15 into a scalar double precision floating-point
 
-     divsd xmm9, xmm12
-     divsd xmm8, xmm13
+     divsd xmm9, xmm12                          ;Divide xmm9 over xmm12
+     divsd xmm8, xmm13                          ;Divide xmm8 over xmm12
 
-     addsd xmm8, xmm9
+     addsd xmm8, xmm9                           ;Add xmm8 over xmm9
 
-     divsd xmm15, xmm8
+     divsd xmm15, xmm8                          ;Divide xmm15 over xmm8 to get the resistance formula answer
 
+     ;Displays the output of the resistance formula
      mov rax, 1
      mov rdi, resistanceOutputPrompt
      movsd xmm0, xmm15
      call printf
      pop rax
 
+     ;Displays whether the user is a computer science major or not prompt to the user
      push qword 0
      mov rax, 0
-     mov rdi, majorPrompt
+     mov rdi, majorPrompt                       ;"Were you a computer science major (y or n)?"
      call printf
      pop rax
 
      push qword 0
-     ;mov rax, 0
-     mov rdi, stringFormat
+     mov rdi, stringFormat                      ;"%s"
      mov rsi, rsp
      call scanf
      pop rax
 
-     mov r15, 'y'
-     cmp rax, r15
-     je programmer
+     mov r15, 'y'                               ;Move 'y' into register r15
+     cmp rax, r15                               ;Compare register rax with r15
+     je programmer                              ;If register rax is y and equal to r15, jump to programmer
 
-     mov r13, 'n'
-     cmp rax, r13
-     je noMajor
-
-
-
+     mov r13, 'n'                               ;Move 'n' into register r13
+     cmp rax, r13                               ;Compare register rax with r13
+     je noMajor                                 ;If register rax is n and equal to r13, jump to noMajor
+     ;============= End section for resistance jump============================================================================================================
 
 
-
-
+     ;============= Begin section for programmer jump==========================================================================================================
      programmer:
+     ;Display exit prompt to the user
      push qword 0
      mov rax, 0
-     mov rdi, exitPrompt
+     mov rdi, exitPrompt                        ;"Thank you.  Please follow the exit signs to the front desk."
      call printf
      pop rax
 
-    ;  mov r14, 88000
-    ;  cvtsi2sd xmm9, r14
-    ;  mov r8, 88
-    ;  cvtsi2sd xmm8, r8
-    ;  mov r15, 100
-    ;  cvtsi2sd xmm15, r15
+     mov rax, 0x40F57C0E147AE148                ;IEE-754 floating-point conversion of 88000.18
+     movq xmm14, rax                            ;Move quadword rax to register xmm14
+     jmp exit                                   ;Jump to exit
+     ;============= End section for programmer jump============================================================================================================
+     
 
-    ;  divsd xmm8, xmm15
-
-    ;  addsd xmm9, xmm8
-
-    ;  movsd xmm0, xmm9
-
-    ;  push qword 0
-    ;  mov rax, 1
-    ;  mov rdi, testPrompt
-    ;  movsd xmm0, xmm9
-    ;  call printf
-    ;  pop rax
-
-    ;  push qword 0
-    ;  mov rax, 0
-    ;  mov rdi, testPrompted
-    ;  call printf
-    ;  pop rax
-    ;  push rdx, 0x47abe071
-    ;  movsd xmm0, [rsp]
-    ;  movsd xmm0, xmm9
-     mov rax, 0x40F57C0E147AE148
-     movq xmm14, rax
-     jmp exit
-
+     ;============= Begin section for noMajor jump=============================================================================================================
      noMajor:
+     ;Display exit prompt to the user
      push qword 0
      mov rax, 0
-     mov rdi, exitPrompt
+     mov rdi, exitPrompt                        ;"Thank you.  Please follow the exit signs to the front desk."
      call printf
      pop rax
 
-     mov rax, 0x4092C07AE147AE14
-     movq xmm14, rax
-     jmp exit
+     mov rax, 0x4092C07AE147AE14                ;IEE-754 floating-point conversion of 1200.12
+     movq xmm14, rax                            ;Move quadword rax to register xmm14
+     jmp exit                                   ;Jump to exit
+     ;============= End section for noMajor jump===============================================================================================================
+     
 
+     ;============= Begin section for chris_sawyer jump========================================================================================================
      chris_sawyer:
+     ;Display exit prompt to the user
      push qword 0
      mov rax, 0
      mov rdi, exitPrompt
      call printf
      pop rax
 
-     mov rax, 0x412E848000000000
-     movq xmm14, rax
-     jmp exit
+     mov rax, 0x412E848000000000                ;IEE-754 floating-point conversion of 1000000
+     movq xmm14, rax                            ;Move quadword rax to register xmm14
+     jmp exit                                   ;Jump to exit
+     ;============= End section for chris_sawyer jump==========================================================================================================
 
-
-
-
-
-
-
-
-
-
-
-
-
+     
+     ;============= Begin section for end jump=================================================================================================================
      exit:
-     ;Restore the registers that were backed up and defined
-    ; movsd xmm0, xmm9
-     movsd xmm0, xmm14
+     movsd xmm0, xmm14                          ;Move register xmm14 to register xmm0 to return the value back to main
      pop rax
 
+     ;Restore the registers that were backed up and defined
      popf                                                        ;Restoring rflags
      pop rbx                                                     ;Restoring rbx
      pop r15                                                     ;Restoring r15
@@ -273,12 +276,5 @@ _first:
      pop rdi                                                     ;Restoring rdi
      pop rbp                                                     ;Restoring rbp
 
-    ;  push qword 0
-    ;  mov rax, 1
-    ;  mov rdi, testPrompt
-    ;  movsd xmm0, xmm9
-    ;  call printf
-    ;  pop rax
-
-
      ret
+     ;============= End section for end jump===================================================================================================================
