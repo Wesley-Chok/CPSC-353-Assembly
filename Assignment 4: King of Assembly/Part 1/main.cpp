70df4161022e69342f8572d89f224bbf09847f48
Wesley-Chok <55215498+Wesley-Chok@users.noreply.github.com>
2021-05-12 23:19:42 -0700

    Update main.cpp

--- Assignment 4: King of Assembly/Part 1/main.cpp
+++ Assignment 4: King of Assembly/Part 1/main.cpp
@@ -1,7 +1,49 @@
+// ****************************************************************************************************************************
+// Program name: "Assignment 4 King of Assembly".  This program demonstrates passing values from main to assembly file,       *
+// calculating arithimetic functions in assembly file and using IEEE 754 conversions.                                         *
+// Copyright (C) 2021 Wesley Chok                                                                                             *
+// This file is part of the software program "King of Assembly".                                                              *
+// King of Assembly is free software: you can redistribute it and/or modify it under the terms of the GNU General             *
+// Public License version 3 as published by the Free Software Foundation.                                                     *
+// King of Assembly is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the             *
+// implied                                                                                                                    *
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.     *
+// A copy of the GNU General Public License v3 is available here:  <https:;www.gnu.org/licenses/>.                            *
+// ****************************************************************************************************************************
+
+
+
+
+//========1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2=========3**
+//
+// Author information
+//  Author name: Wesley Chok
+//  Author email: chokwj@csu.fullerton.edu
+//
+// Program information
+//  Program name: Assignment 4 King of Assembly
+//  Programming languages: One modules in C++ and one modules assembly.
+//  Date program began: 2021-April-26
+//  Date of last update: 2021-May-12
+//  Date of reorganization of comments: 2021-May-12
+//  Files in this program: main.cpp, interview.asm
+//  Status: Finished.  The program was tested extensively with no errors in Xubuntu19.04.
+//
+// Purpose
+//  This serves as the main C++ file for this program. The tasks this C++ file are to complete are welcoming the user,
+//  asking the user various prompts and passing the values to the assembly file, and retrieving values from the assembly file
+//  and properly displaying them
+// This file
+//   File name: main.cpp
+//   Language: X86 with Intel syntax.
+//   Max page width: 132 columns
+//   Assemble: g++ -c -m64 -Wall -fno-pie -no-pie -o main.o main.cpp
+
+
+// ===== Begin code area ================================================================================================
+
 #include <iostream>
 #include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
 using namespace std;
 extern "C" double _first(char[], double);
 
