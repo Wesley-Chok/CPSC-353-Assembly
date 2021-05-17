361c2ca52da6f7ee3c50e98d8df2a41eb3d292be
Wesley-Chok <55215498+Wesley-Chok@users.noreply.github.com>
2021-05-17 12:06:59 -0700

    Update r.sh

--- Assignment 4: King of Assembly/Part 1/r.sh
+++ Assignment 4: King of Assembly/Part 1/r.sh
@@ -11,7 +11,7 @@ echo "Compile rectangle.cpp"
 g++ -c -m64 -Wall -fno-pie -no-pie -o main.o main.cpp
 
 echo  "Assemble perimeter.asm"
-nasm -f elf64 -l perimeter.lis -o interview.o interview.asm
+nasm -f elf64 -l interview.lis -o interview.o interview.asm
 
 echo "Link the object files using g++"
 g++ -m64 -fno-pie -no-pie -o assembly.out main.o interview.o -std=c++17 
